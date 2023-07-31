4d7d5eaa6616df9bd3bc3cf30164cd274859c2f0
r_cruz <r_cruz@farmacity.com.ar>
2023-07-31 16:25:19 -0500

    Update: Database queries

--- events/events_service.py
+++ events/events_service.py
@@ -13,16 +13,16 @@ def get_events():
 
 
 def get_event_by_id(*args):
-    response = query(f"SELECT * FROM users where id = ?;", *args)
+    response = query(f"SELECT * FROM events where id = ?;", *args)
     if types.NoneType == type(response):
         response = [False]
     return response[0]
 
 
 def delete_event(index):
-    query(f"DELETE from users WHERE id = {index};")
+    query(f"UPDATE events set 'deleted'=true WHERE id = {index};")
     return {
-                "message": "User deleted",
+                "message": "Event deleted",
             }, 200
 
 
@@ -35,20 +35,7 @@ def create_event(payload):
 
 def update_event(p_id: int, payload):
     value_set = utilities.payload_to_valueset(payload)
-    query(f"UPDATE users set {value_set} WHERE id = {p_id};")
+    query(f"UPDATE events set {value_set} WHERE id = {p_id};")
     return {"message": "User updated"}, 200
 
 
-def update_password(index: int, password: str, new_password: str):
-    hashed_password = query(f"SELECT password FROM users where id = ?;", index)
-    matched = bcrypt.checkpw(password, hashed_password[0]['password'])
-    if matched:
-        new_password = bcrypt.hashpw(new_password, salt)
-        query(f"UPDATE users set password='{new_password}' WHERE id = {index};")
-    return matched
-
-
-def signin(username, password):
-    # TODO: JWT Implementation
-    q_response = query(f"SELECT username, password FROM users WHERE username='{username}'")
-    return q_response[0]['username'] == username and bcrypt.checkpw(password, q_response[0]['password'])
